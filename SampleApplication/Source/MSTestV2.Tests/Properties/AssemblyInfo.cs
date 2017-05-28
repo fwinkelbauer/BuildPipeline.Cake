75f7dfd624cefeca4f718bac6e1ace6c70c3ff1c
Florian Winkelbauer <mail@florianwinkelbauer.com>
2017-05-28 11:32:09 +0200

    Upgrade to VS2017 and add MSTest2 test project

---
+++ SampleApplication/Source/MSTestV2.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTestV2.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTestV2.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a521613-94dc-4dc2-9468-6ba0d07a70e7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SampleApplication/Source/MSTestV2.Tests/SanityTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestV2.Tests
+{
+    [TestClass]
+    public class SanityTest
+    {
+        [TestMethod]
+        public void TestSanity()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
